60eb624d845434081eec6e684bbc9865d17f503d
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-05-31 17:46:27 +0800

    Update VideoCV.py

--- VideoCV.py
+++ VideoCV.py
@@ -1,44 +1,18 @@
 import cv2
 
-
-WHITE = [255, 255, 255]
-face_cascade = cv2.CascadeClassifier('Haar/haarcascade_frontalcatface.xml')
-eye_cascade = cv2.CascadeClassifier('Haar/haarcascade_eye.xml')
-
-
-def draw_box(Image, x, y, w, h):
-    cv2.line(Image, (x, y), (x + int(w / 5), y), WHITE, 2)
-    cv2.line(Image, (x + int((w / 5) * 4), y), (x + w, y), WHITE, 2)
-    cv2.line(Image, (x, y), (x, y + int(h / 5)), WHITE, 2)
-    cv2.line(Image, (x + w, y), (x + w, y + int(h / 5)), WHITE, 2)
-    cv2.line(Image, (x, (y + int(h / 5 * 4))), (x, y + h), WHITE, 2)
-    cv2.line(Image, (x, (y + h)), (x + int(w / 5), y + h), WHITE, 2)
-    cv2.line(Image, (x + int((w / 5) * 4), y + h), (x + w, y + h), WHITE, 2)
-    cv2.line(Image, (x + w, (y + int(h / 5 * 4))), (x + w, y + h), WHITE, 2)
-
-
 class VideoCamera(object):
     def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
         self.video = cv2.VideoCapture(0)
-
+        
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
         success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-
+        # so we must encode it into JPEG in order to correctly display the video stream.
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in faces:
-           gray_face = cv2.resize((gray[y:y + h, x:x + w]), (110, 110))
-            eyes = eye_cascade.detectMultiScale(gray_face)
-            for (ex, ey, ew, eh) in eyes:
-
-                draw_box(gray, x, y, w, h)
-
-        ret, jpeg = cv2.imencode('.jpg', gray)
-
+        #roi= gray[100:300,150:350]        
+        ret, jpeg = cv2.imencode('.jpg', roi)
         return jpeg.tobytes()
